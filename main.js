35413d4439c3fa65ae8fe7db281bf727a141a484
ZokirovZubayr <zokirovzubayr@gmail.com>
2024-03-08 04:11:06 -0800

    I want to learn so I add this commit

---
+++ main.js
@@ -0,0 +1,6 @@
+let number = promt =Number( " Randomda chiqqan son 57 " );
+alert(name = " Randomdagi chiqqan son 57 " );
+let Lastnumber = promt =Number( " Randomda chiqqan son 57 " );
+alert(Lastnumber = " 57 : fizz " );
+let Scriptnumber = promt =Number( " Randomda chiqqan son 57 " );
+alert(Scriptnumber = " 57 chiqqan son fizz ham emas fizzbuzz ham emas " );
